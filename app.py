45afdd828f140b6838b3081b027d99f55b2c5a25
Jay Punekar <jaypunekar01@gmail.com>
2023-01-27 06:13:13 +0530

    changed text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return "Starting ML Project"
+    return "This is TESTING CI/CD pipeline"
 
 if __name__=='__main__':
     app.run(debug=True)
