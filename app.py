6c677f87b6f5a1fa13d347e973292644f2cb2f20
Jay Punekar <jaypunekar01@gmail.com>
2023-01-28 17:33:56 +0530

    logger and exception compleated

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import sys
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    try:
+        raise Exception("We testing custom exception")
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.info(housing.error_message)
+    
+    logging.info("We are testing logging module")
     return "This is TESTING CI/CD pipeline"
 
 if __name__=='__main__':
