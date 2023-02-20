b33bf7561a86dbef1f28376086484dfb31a79c54
Jay Punekar <jaypunekar01@gmail.com>
2023-02-20 15:38:11 +0530

    updated data validation

--- demo.py
+++ demo.py
@@ -1,11 +1,14 @@
 from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
+from housing.config.configuration import Configuration
 
 def main():
     try:
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
+        # pipeline = Pipeline()
+        # pipeline.run_pipeline()
+        data_validation_config = Configuration()
+        print(data_validation_config.get_data_validation_config())
     except Exception as e:
         logging.error(f"{e}")
         print(e)
