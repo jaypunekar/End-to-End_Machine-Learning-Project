09a96e6c057f4ad7b89db42ae0dbcb015c5c232c
Jay Punekar <jaypunekar01@gmail.com>
2023-02-25 02:43:19 +0530

    Compleated Data Transformation

--- demo.py
+++ demo.py
@@ -2,13 +2,18 @@ from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
 from housing.config.configuration import Configuration
+from housing.component.data_transformation import DataTransformation
 
 def main():
     try:
-        # pipeline = Pipeline()
-        # pipeline.run_pipeline()
-        data_validation_config = Configuration()
-        print(data_validation_config.get_data_transformation_config())
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
+        # sfp = r"C:\Users\91973\Downloads\ML_Projects\Machine-Learning-Project\config\schema.yaml"
+        # fp = r"C:\Users\91973\Downloads\ML_Projects\Machine-Learning-Project\housing\artifact\data_ingestion\2023-02-21-17-50-40\ingested_data\train\housing.csv"
+        # df = DataTransformation.load_data(file_path = fp, schema_file_path =sfp)
+        # print(df.astype)
+        # print(df.columns)
+        
     except Exception as e:
         logging.error(f"{e}")
         print(e)
